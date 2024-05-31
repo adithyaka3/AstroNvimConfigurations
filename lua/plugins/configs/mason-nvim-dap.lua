ac98b6491b4e1e4af1530d3a501542480ef68141
Adithya <adithyaka@iisc.ac.in>
2024-05-31 20:53:00 +0530

    Added debug config
    Nearly screwed up the debugging process of python, c, and c++ however
    finally set it up!

--- lua/plugins/configs/mason-nvim-dap.lua
+++ lua/plugins/configs/mason-nvim-dap.lua
@@ -1,5 +1,12 @@
 -- TODO: remove unnecessary file in AstroNvim v4
 return function(_, opts)
-  local mason_nvim_dap = require "mason-nvim-dap"
-  mason_nvim_dap.setup(opts)
+    local mason_nvim_dap = require("mason-nvim-dap")
+    mason_nvim_dap.setup(opts)
 end
+
+-- return {
+--     "jay-babu/mason-nvim-dap.nvim",
+--     opts = {
+--         ensure_installed = { "python" },
+--     },
+-- }
