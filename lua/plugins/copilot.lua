5c70e9877afae05ce6f7628b0c9a1d28ac4b0422
Adithya <adithyaka@iisc.ac.in>
2024-07-09 22:14:31 +0530

    Changed Keybindings
    The keybindings have been changed in such a way that <Tab> now completes
    the copilot autocompletion rather than the cmp completion

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,17 @@
 return {
-    {
-        "github/copilot.vim",
-        event = "InsertEnter",
-        config = function()
-            vim.cmd("source ~/.config/nvim/extra_conf.vim")
-        end,
-    },
+	{
+		"github/copilot.vim",
+		event = "VimEnter",
+		config = function()
+			vim.cmd("source ~/.config/nvim/extra_conf.vim")
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_filetypes = { ["*"] = true }
+			vim.api.nvim_set_keymap(
+				"i",
+				"<Tab>",
+				'copilot#Accept("<Tab>")',
+				{ silent = true, expr = true, script = true }
+			)
+		end,
+	},
 }
